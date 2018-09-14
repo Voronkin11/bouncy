309ee551e96b035533cb9b8b77e752df7f8f22c2
voronkin11 <vvadk11@gmail.com>
2018-09-14 19:43:17 +0300

    add isotope filtering

--- js/isotope.js
+++ js/isotope.js
@@ -1,18 +1,22 @@
-// var $grid = $('.grid').isotope({
-//   itemSelector: '.grid__item',
-//   masonry: {
-//   columnWidth: 270,
-//   gutter: 10,
-//   horizontalOrder: true
+(function(){
+	'use strict'
+    var $container = $('.grid').isotope({
+		itemSelector: '.grid__item',
+		masonry: {
+			columnWidth: 270,
+			gutter: 10,
+			horizontalOrder: true
+		}
+	});
 
-// }
-// });
-
-var isot = new Isotope( '.grid', {
-  itemSelector: '.grid__item',
-  masonry: {
-  columnWidth: 270,
-  gutter: 10,
-  horizontalOrder: true
-}
-});
+	$('.button').click(function(){
+		var $this = $(this);
+		if ( !$this.hasClass('is-checked') ) {
+			$this.parents('.filters-button-group').find('.is-checked').removeClass('is-checked');
+			$this.addClass('is-checked');
+		}
+	var selector = $this.attr('data-filter');
+		$container.isotope({  itemSelector: '.grid__item', filter: selector });
+		return false;
+	});
+})();
