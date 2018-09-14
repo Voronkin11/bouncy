b90d4db59408ffd688f9c349835b05bdebc6c603
voronkin11 <vvadk11@gmail.com>
2018-09-14 16:20:11 +0300

    add isotope masonry grid

---
+++ js/isotope.js
@@ -0,0 +1,18 @@
+// var $grid = $('.grid').isotope({
+//   itemSelector: '.grid__item',
+//   masonry: {
+//   columnWidth: 270,
+//   gutter: 10,
+//   horizontalOrder: true
+
+// }
+// });
+
+var isot = new Isotope( '.grid', {
+  itemSelector: '.grid__item',
+  masonry: {
+  columnWidth: 270,
+  gutter: 10,
+  horizontalOrder: true
+}
+});
